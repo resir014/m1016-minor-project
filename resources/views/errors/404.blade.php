b3d504407d16a2548a1e233e8c6499aa319a2c61
Resi Respati <resir014@gmail.com>
2015-10-22 16:17:48 +0700

    Updated HTTP Error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('content')
+<div class="container">
+    <h2>404: Page not found.</h2>
+    <p>
+        We couldn't find the page you're looking for. <a href="/">Go back home?</a>
+    </p>
+</div>
+@stop
