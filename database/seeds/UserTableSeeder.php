7d8a552b78668d6058fe4aba3147dcff6f49a16d
Resi Respati <resir014@gmail.com>
2016-01-02 10:44:07 +0700

    [ModelFactory] Generate 3 test admins and 5 test lecturers

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -17,37 +17,30 @@ class UserTableSeeder extends Seeder
         // Faker is a library to generate dummy data.
         $faker = Faker\Factory::create();
 
-        factory(App\User::class, 'master-admin')->create([
-            'userable_id' => 'A0001',
-        ]);
-
-        factory(App\Admin::class, 'master-admin')->create([
-            'id' => 'A0001',
-        ]);
-
-        for ($i = 0; $i < 3; $i++) {
-            $randomNumberTemp = $faker->unique()->numberBetween(1,9999);
-
+        for ($i = 1; $i <= 3; $i++) {
             factory(App\User::class, 'admin')->create([
-                'email' => 'test.admin.' . sprintf('%04d', $randomNumberTemp) . '@example.com',
-                'userable_id' => 'A' . sprintf('%04d', $randomNumberTemp),
+                'email' => 'test.admin.' . sprintf('%04d', $i) . '@example.com',
+                'userable_id' => 'A' . sprintf('%04d', $i),
             ]);
 
             factory(App\Admin::class, 'admin')->create([
-                'id' => 'A0001' . sprintf('%04d', $randomNumberTemp),
+                'id' => 'A0001' . sprintf('%04d', $i),
             ]);
         }
 
-        factory(App\User::class, 'test-lecturer')->create([
-            'userable_id' => 'D0001',
-        ]);
+        for ($i = 1; $i <= 5; $i++) {
+            factory(App\User::class, 'test-lecturer')->create([
+                'email' => 'test.lecturer.' . sprintf('%04d', $i) . '@example.com',
+                'userable_id' => 'D' . sprintf('%04d', $i),
+            ]);
 
-        factory(App\Lecturer::class, 'test-lecturer')->create([
-            'id' => 'D0001',
-        ]);
+            factory(App\Lecturer::class, 'test-lecturer')->create([
+                'id' => 'D' . sprintf('%04d', $i),
+            ]);
+        }
 
         for ($i = 0; $i < 15; $i++) {
-            $randomNumberTemp = $faker->unique()->numberBetween(1,9999);
+            $randomNumberTemp = $faker->unique()->numberBetween(6,9999);
 
             factory(App\User::class, 'lecturer')->create([
                 'email' => 'test.lecturer.' . sprintf('%04d', $randomNumberTemp) . '@example.com',
