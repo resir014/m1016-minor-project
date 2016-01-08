8692e619d2ea13f5cf6446b1a2e927b1188e762b
Resi Respati <resir014@gmail.com>
2016-01-08 18:40:20 +0700

    [students] Fixed forms

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -30,7 +30,39 @@
         'route' => 'students.store'
     ]) !!}
 
-    @include('students.partials.form', ['buttonText' => 'Save'])
+    <div class="form-group">
+        {!! Form::label('id', 'Student ID', ['class' => 'control-label']) !!}
+        {!! Form::text('id', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name', ['class' => 'control-label']) !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('admission_year', 'Admission Year', ['class' => 'control-label']) !!}
+        {!! Form::text('admission_year', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('birth_date', 'Birth Date', ['class' => 'control-label']) !!}
+        {!! Form::input('date', 'birth_date', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password', 'Verification Password', ['class' => 'control-label']) !!}
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('password_confirmation', 'Confirm Password', ['class' => 'control-label']) !!}
+        {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::submit('Save', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
 
     {!! Form::close() !!}
 </div>
