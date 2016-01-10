0e0355e3d336482764a65b83e14c050b5b4fc622
Resi Respati <resir014@gmail.com>
2016-01-11 03:26:56 +0700

    [Excel] Fixed exported columns

--- app/Http/Controllers/ExcelExportController.php
+++ app/Http/Controllers/ExcelExportController.php
@@ -13,12 +13,22 @@ class ExcelExportController extends Controller
     {
         $excel = \App::make('excel');
 
-        \Excel::create('Filename', function($excel) {
+        $excel->create('Filename', function ($excel) {
+            $excel->sheet('Sheet 1', function ($sheet) {
+                $query = \DB::table('fixed_schedules')->select('id', 'schedule_draft_id', 'lecturer_id', 'course_id', 'room_id', 'day', 'shift')->get();
 
-            $excel->sheet('Sheet 1', function($sheet) {
+                $sheet->fromArray(json_decode(json_encode($query), true));
+            });
+        })->download('xlsx');
+    }
+
+    public function getClassRealisation() {
+        $excel = \App::make('excel');
+
+        \Excel::create('Filename', function ($excel) {
+            $excel->sheet('Sheet 1', function ($sheet) {
                 $sheet->fromArray(FixedSchedule::all()->toArray());
             });
-
         })->download('xlsx');
     }
 }
